0988f3e50170561fb989ceb25e2ac353509b50d8
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-11-11 16:45:44 -0300

    fix: Call setup_logging function with parentheses in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.extract.extract import extract_data
 from src.log.log_config import setup_logging
 from src.modules.paths import RAW_FILE_PATH
 
-setup_logging
+setup_logging()
 
 def main():
 
@@ -20,7 +20,7 @@ def main():
         logging.warning(f'[Pipeline] The extracted data is empty. Check the source file')
         return
     
-    logging.info(f'[Pipeline] Data extraction complete')
+    logging.info(f'[Pipeline] Data extraction complete!')
     logging.info(f'[Pipeline] First 5 rows of the extracted data:')
     logging.info(f'\n' + df_raw.head().to_string(index=False))
     logging.info('='* 50)
